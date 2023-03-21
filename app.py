0335a613ce61892c54f630075ae9022ceb4f2d25
Xiaoyan(Sharon) MU <xmu@esri.com>
2023-03-21 16:42:56 +0800

    Week2 for Coordinates
    Week2 for Coordinates

--- app.py
+++ app.py
@@ -8,29 +8,56 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+# @app.route("/", methods=("GET", "POST"))
+# def index():
+#     if request.method == "POST":
+#         animal = request.form["animal"]     # the code that sends the actual API request
+#         response = openai.Completion.create(
+#             model="text-davinci-003",
+#             prompt=generate_prompt(animal),
+#             temperature=0.6,
+#         )
+#         return redirect(url_for("index", result=response.choices[0].text))
+#
+#     result = request.args.get("result")
+#     return render_template("index.html", result=result)
+
+# the function that generates the prompt that we were using above.
+# def generate_prompt(animal):
+#     return """Suggest three names for an animal that is a superhero.
+#
+#         Animal: Cat
+#         Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+#         Animal: Dog
+#         Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+#         Animal: {}
+#         Names:""".format(
+#         animal.capitalize()
+#     )
+
+
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]     # the code that sends the actual API request
+        place = request.form["place"]  # the code that sends the actual API request
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(place),
             temperature=0.6,
+            max_tokens=50
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-# the function that generates the prompt that we were using above.
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(place):
+    return """Suggest the latitude and longitude in JSON format of the given place.
+    
+    Place: Beijing
+    latlon: "city": "Beijing","latitude": 39.9042,"longitude": 116.4074
+    Place: Shanghai
+    latlon: "city": "Shanghai","latitude": 31.2304,"longitude": 121.4737
+    Place: {}
+    Latlon:""".format(place.capitalize())
