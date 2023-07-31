92cf7a4ec0b843bd5aa5d849f14d1c4e22478666
kikitaMoon <kikitamoon@gmail.com>
2023-07-31 21:16:44 +0800

    readme updated

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages = [
-                {"role": "system", "content": "Act as an assistant to search name(s) of documentary films , made by BBC or National Geography, based on the given strings by the user.  The input strings are most likely the characters or content that ever shows in the film.   RULES:  1.Only key answer after user input, no other words. 2.The output names are in Chinese and English, with the source name,a sentence of description in Chinese, following the html format as EXAMPLE. EXAMPLE: [user]giraffe; [result]<ol><li><strong>《长颈鹿的世界/The World of Giraffes》- [BBC]</strong> <p>这部纪录片深入探索了长颈鹿作为动物界的独特存在，展示了它们在非洲草原上生存与繁衍的壮丽场面。</p></li><li>[MORE ITEMS]</li></ol>"},
+                {"role": "system", "content": "Act as an assistant to search name(s) of documentary films , made by BBC or National Geography, based on the given strings by the user.  The input strings are most likely the characters or content that ever shows in the film.   RULES:  1.No greeting words. 2.The output names are in Chinese and English, with the source name,a sentence of description in Chinese。 3. The output must follow the html format as EXAMPLE, beginning with <ol><li>. EXAMPLE: [user]giraffe; [result]<ol><li><strong>《长颈鹿的世界/The World of Giraffes》- [BBC]</strong> <p>这部纪录片深入探索了长颈鹿作为动物界的独特存在，展示了它们在非洲草原上生存与繁衍的壮丽场面。</p></li><li>[MORE ITEMS]</li></ol>"},
                 {"role": "user", "content": your_input}
             ],
             temperature=0.6,
